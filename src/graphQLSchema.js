f2f0e97d9fc8fd1bee72a9838035ff2d8e2679e4
nicolas larranaga <nlarranagac@unal.edu.co>
2018-04-22 23:16:58 -0500

    ldap

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -22,9 +22,15 @@ import {
 	gradesTypeDef
 } from './grades/typeDefs';
 
+import {
+	authMutations,
+	authTypeDef
+} from './auth/typeDefs';
+
 import studentsResolvers from './students/resolvers';
 import coursesResolvers from './courses/resolvers';
 import gradesResolvers from './grades/resolvers';
+import authResolvers from './auth/resolvers';
 
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
@@ -32,7 +38,8 @@ const mergedTypeDefs = mergeSchemas(
 		'scalar JSON',
 		studentsTypeDef,
 		coursesTypeDef,
-		gradesTypeDef
+		gradesTypeDef,
+		authTypeDef
 	],
 	[
 		studentsQueries,
@@ -42,7 +49,8 @@ const mergedTypeDefs = mergeSchemas(
 	[
 		studentsMutations,
 		coursesMutations,
-		gradesMutations
+		gradesMutations,
+		authMutations
 	]
 );
 
@@ -52,6 +60,8 @@ export default makeExecutableSchema({
 	resolvers: merge(
 		{ JSON: GraphQLJSON }, // allows scalar JSON
 		studentsResolvers,
-		coursesResolvers
+		coursesResolvers,
+		gradesResolvers,
+		authResolvers
 	)
 });
